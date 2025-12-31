fe78c9fd5c2fa6549411bbd52ff31c85d3b4566a
mzal <58111339+mzwrt@users.noreply.github.com>
2025-12-31 20:59:17 +0800

    Delete DIY/diy1.sh

--- DIY/diy1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#=================================================
-# MZwrt script
-#================================================= 
-
-
-##添加自己的插件库
-echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package_Lite.git" >> feeds.conf.default
